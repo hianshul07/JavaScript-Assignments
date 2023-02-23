50cc80ffe2c8850a1b688ff649c0fef35835720d
Anshul Kumar <hianshul07@gmail.com>
2023-02-23 21:46:35 +0530

    02 done

---
+++ 02 Ouestion/interpolation.js
@@ -0,0 +1,13 @@
+var firstName = "Anshul";
+var lastName = "Kumar";
+var maritalStatus = "Unmarried";
+var country = "India";
+var age = 24;
+
+console.log(
+  `First name: ${firstName},
+    Last name: ${lastName},
+    Marital Status: ${maritalStatus},
+    Country of Residence: ${country},
+    Age: ${age}`
+);
