6f3c8568687043e8f2282b648b27c4ea92f74512
Anshul Kumar <hianshul07@gmail.com>
2023-02-23 20:17:40 +0530

    01 done

---
+++ 01 Ouestion/variable.js
@@ -0,0 +1,25 @@
+// Int
+var int1 = 7;
+console.log(int1);
+
+// String
+var string1 = "This is a String";
+console.log(string1);
+
+// Undefined
+var unDefined1;
+// console.log(undefined1);
+
+// null
+var thisIsNull = null;
+console.log(thisIsNull);
+
+// boolean
+
+    // true
+    var thisIsBool = true;
+    console.log(thisIsBool);
+
+    // false
+    var thisIsBool = true;
+    console.log(!thisIsBool);
