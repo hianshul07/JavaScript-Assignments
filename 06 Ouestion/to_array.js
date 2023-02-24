ce5d4d1df38b4eb83a5327b50daca888114dc873
Anshul Kumar <hianshul07@gmail.com>
2023-02-24 15:31:12 +0530

    06 done

---
+++ 06 Ouestion/to_array.js
@@ -0,0 +1,6 @@
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+var giants = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+var newArry = giants.split(" ");
+console.log(newArry);
