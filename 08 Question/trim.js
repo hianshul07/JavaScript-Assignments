276223aaa86b7f47a5ead9c45ae1dd7ba7aa6fb8
Anshul Kumar <hianshul07@gmail.com>
2023-02-26 23:35:28 +0530

    08 done

---
+++ 08 Question/trim.js
@@ -0,0 +1,8 @@
+// Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+const strngSpaces = "   this is a string with spaces at the start as well as at the end    ";
+console.log(strngSpaces);
+
+// trim use
+const strngTrimmed = strngSpaces.trim();
+console.log(strngTrimmed);
