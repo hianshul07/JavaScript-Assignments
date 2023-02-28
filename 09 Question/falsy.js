b237ce972a63fd0488c72e9876401f712e8154ad
Anshul Kumar <hianshul07@gmail.com>
2023-02-28 13:06:22 +0530

    09 done

---
+++ 09 Question/falsy.js
@@ -0,0 +1,16 @@
+//  Write three JavaScript statement example which provide falsy value.
+
+// 1
+if (100 <= 90) {
+    console.log(true);
+  } else console.log(false);
+  
+  // 2
+  if (10 === "10") {
+    console.log(true);
+  } else console.log(false);
+  
+  // 3
+  if (10 + 25 === 30) {
+      console.log(true);
+  } else console.log(false);
---
+++ 09 Question/truthy.js
@@ -0,0 +1,17 @@
+//  Write three JavaScript statement example which provide truthy value
+
+
+// 1
+if (100 > 90) {
+  console.log(true);
+} else console.log(false);
+
+// 2
+if (10 == "10") {
+  console.log(true);
+} else console.log(false);
+
+// 3
+if (10 + 20 == 30) {
+    console.log(true);
+} else console.log(false);
