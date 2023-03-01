a4ee16f555fd91cd258e8635c602eaced320683f
Anshul Kumar <hianshul07@gmail.com>
2023-03-01 20:18:05 +0530

    10 done

---
+++ 10 Question/compare.js
@@ -0,0 +1,70 @@
+// Figure out the result of the following comparison expression first without using console.log().
+
+// self-Assessed
+
+//  4 > 3 is true
+//  4 >= 3 is true
+//  4 < 3 is false
+//  4 <= 3 is false
+//  4 == 4 is true
+//  4 === 4 is true
+//  4 != 4 is false
+//  4 !== 4 is false
+//  4 != '4' is false
+//  4 == '4' is true
+//  4 === '4' is false
+
+// 1
+if (4 > 3) {
+  console.log(true);
+} else console.log(false);
+
+// 2
+if (4 >= 3) {
+  console.log(true);
+} else console.log(false);
+
+// 3
+if (4 < 3) {
+  console.log(true);
+} else console.log(false);
+
+// 4
+if (4 <= 3) {
+  console.log(true);
+} else console.log(false);
+
+// 5
+if (4 == 4) {
+  console.log(true);
+} else console.log(false);
+
+// 6
+if (4 === 4) {
+  console.log(true);
+} else console.log(false);
+
+// 7
+if (4 != 4) {
+  console.log(true);
+} else console.log(false);
+
+// 8
+if (4 !== 4) {
+  console.log(true);
+} else console.log(false);
+
+// 9
+if (4 != '4') {
+    console.log(true);
+  } else console.log(false);
+
+// 10
+if (4 == '4') {
+    console.log(true);
+  } else console.log(false);
+
+// 11
+if (4 === '4') {
+    console.log(true);
+  } else console.log(false);
