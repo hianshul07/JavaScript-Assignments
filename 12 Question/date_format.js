bb442badbee78c3f41229ffafbe286d35022ffd1
Anshul Kumar <hianshul07@gmail.com>
2023-03-04 13:54:28 +0530

    12 done

---
+++ 12 Question/date_format.js
@@ -0,0 +1,10 @@
+const date = new Date();
+
+//  YYYY-MM-DD HH:mm
+console.log(date.toLocaleString('en-ZA'));
+
+//  DD-MM-YYYY HH:mm
+console.log(date.toLocaleString('es-CL'));
+
+//  DD/MM/YYYY HH:mm
+console.log(date.toLocaleString('fr-FR'));
