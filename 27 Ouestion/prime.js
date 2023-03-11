3a630a9048308f3781e420a486fc305c3916c927
Anshul Kumar <hianshul07@gmail.com>
2023-03-11 23:35:10 +0530

    prime numbers done

---
+++ 27 Ouestion/prime.js
@@ -0,0 +1,17 @@
+var start;
+
+for (start = 1; start <= 100; start++) {
+	for (var i = 2; i < start; i++) {
+		if (start % i == 0) {
+			break;
+		}
+			// console.log(start);
+		
+	}
+    if (start == i) {
+        console.log(start);
+    }
+	// console.log(start);
+
+	// console.log(start);
+}
