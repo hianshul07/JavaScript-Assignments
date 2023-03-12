82fbad700342efaa637791a15d0e4732b5a9a5a8
Anshul Kumar <hianshul07@gmail.com>
2023-03-12 15:44:35 +0530

    new logic used

--- 27 Ouestion/prime.js
+++ 27 Ouestion/prime.js
@@ -1,14 +1,17 @@
 var start;
 
 for (start = 1; start <= 100; start++) {
+	var check = 1
 	for (var i = 2; i < start; i++) {
 		if (start % i == 0) {
+			check = 0
+			// console.log(check);
 			break;
 		}
 			// console.log(start);
 		
 	}
-    if (start == i) {
+    if (check == 1) {
         console.log(start);
     }
 	// console.log(start);
