f6abe6d905d9b2c7448a90f93d7472e891321f3e
Anshul Kumar <hianshul07@gmail.com>
2023-03-12 15:54:20 +0530

    some code removed

--- 27 Ouestion/prime.js
+++ 27 Ouestion/prime.js
@@ -1,20 +1,14 @@
 var start;
 
 for (start = 1; start <= 100; start++) {
-	var check = 1
+	var check = 1;
 	for (var i = 2; i < start; i++) {
 		if (start % i == 0) {
-			check = 0
-			// console.log(check);
+			check = 0;
 			break;
 		}
-			// console.log(start);
-		
 	}
-    if (check == 1) {
-        console.log(start);
-    }
-	// console.log(start);
-
-	// console.log(start);
+	if (check == 1) {
+		console.log(start);
+	}
 }
