52a28fe01985fd91203ade8578782703a7109e1e
Anshul Kumar <hianshul07@gmail.com>
2023-03-17 16:59:25 +0530

    added clear button and rearranged code

--- 30 Question/calculator.js
+++ 30 Question/calculator.js
@@ -1,28 +1,3 @@
-// var number1 = parseInt(document.getElementById("first")).value
-// var number2 = parseInt(document.getElementById("second")).value
-
-// function check() {
-// 	var hhe = number1 + number2;
-
-// 	document.getElementById("hehe").innerHTML = `${hhe}`;
-// }
-
-// var num1 = parseInt(document.getElementById("first").value);
-// var num2 = parseInt(document.getElementById("second").value);
-
-// var addition = num1 + num2;
-// var subtraction = num1 - num2;
-// var multiplication = num1 * num2;
-// var division = num1 / num2;
-
-// document.getElementById("add").innerHTML = `${addition}`;
-// document.getElementById("sub").innerHTML = `${subtraction}`;
-// document.getElementById("multi").innerHTML = `${multiplication}`;
-// document.getElementById("div").innerHTML = `${division}`;
-
-// var num1 = parseInt(document.getElementById("first").value);
-// var num2 = parseInt(document.getElementById("second").value);
-
 function addition() {
 	let num1 = parseInt(document.getElementById("first").value);
 	let num2 = parseInt(document.getElementById("second").value);
@@ -59,6 +34,9 @@ function division() {
 	document.getElementById("result").innerHTML = `Result is: ${division}`;
 }
 
-function clear() {
-	parseInt(document.getElementById("input").value) = "";
-}
+var button = document.getElementById("clear")
+
+button.addEventListener("click", function() {
+	document.getElementById("first").value = ""
+	document.getElementById("second").value = ""
+})
