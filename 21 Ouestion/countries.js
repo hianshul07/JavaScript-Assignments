826e636c6a512662264063e73d3accd9675db897
Anshul Kumar <hianshul07@gmail.com>
2023-03-17 19:41:35 +0530

    21 done

---
+++ 21 Ouestion/countries.js
@@ -0,0 +1,4 @@
+const countries = ["China", "India", "Ethiopia", "Georgia", "Egypt", "Alexandria"]
+console.log(countries.includes("Ethiopia"))
+console.log(countries[2].toUpperCase())
+console.log(countries);
