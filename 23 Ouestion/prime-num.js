13ee6da4a7a3ff163c57d6d9410812e673d4ccae
Anshul Kumar <hianshul07@gmail.com>
2023-03-19 22:28:40 +0530

    changes

---
+++ 23 Ouestion/prime-num.js
@@ -0,0 +1,13 @@
+var check = 22;
+
+console.log("check");
+
+for (var i = 1; i < check; i++) {
+	console.log("not prime");
+		// console.log("hehe");
+		break;
+  }
+    // console.log();
+    console.log("is prime");
+
+
