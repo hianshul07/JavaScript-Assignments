3ef904b6d6e02dc1571be163381393b21dfaa197
Anshul Kumar <hianshul07@gmail.com>
2023-03-19 22:35:34 +0530

    solution img added

--- 23 Ouestion/prime-num.js
+++ 23 Ouestion/prime-num.js
@@ -1,14 +1,14 @@
-var check = 12;
+var check = 17;
 
-console.log("check");
+// console.log("check");
 
 for (var i = 2; i < check; i++) {
 	if (check % i === 0) {
-		console.log("not prime");
+		console.log("The number is not prime");
 		// console.log("hehe");
 		break;
 	} else {
-		console.log("is prime");
+		console.log("The number is prime");
     break
 	}
 }
